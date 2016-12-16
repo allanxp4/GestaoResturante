5929782cb6c16183c4cf4c9c0b9bdbff0cbf69b9
Allan Araujo <allanxp4@gmail.com>
2016-12-16 14:27:00 -0200

    Golden Master

--- back/Restaurante/Restaurante/ViewModels/MesaViewModel.cs
+++ back/Restaurante/Restaurante/ViewModels/MesaViewModel.cs
@@ -15,7 +15,7 @@ namespace Potatotech.GestaoRestaurante.Web.ViewModels
         public int Capacidade { get; set; }
         public bool Ocupada { get; set; }
         public int AmbienteId { get; set; }
-
+        
         public virtual Ambiente Ambiente { get; set; }
         public virtual ICollection<Conta> Conta { get; set; }
 
