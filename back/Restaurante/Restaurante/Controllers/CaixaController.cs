875d9045ca57ee0b09988b42ca1498b87a0ffe8d
Allan Araujo <allanxp4@gmail.com>
2016-12-15 22:29:39 -0200

    Mudanças no caixa

--- back/Restaurante/Restaurante/Controllers/CaixaController.cs
+++ back/Restaurante/Restaurante/Controllers/CaixaController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
 using AutoMapper;
 using Potatotech.GestaoRestaurante.Persistencia.UnitsOfWork;
@@ -9,14 +11,19 @@ using Potatotech.GestaoRestaurante.Web.ViewModels;
 
 namespace Potatotech.GestaoRestaurante.Web.Controllers
 {
-   
+
     public class CaixaController : Controller
     {
         #region PRIVATE
+
         public UnitOfWork _unit = new UnitOfWork();
+
         #endregion PRIVATE
+
         // GET: Caixa
+
         #region GET
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -32,14 +39,36 @@ namespace Potatotech.GestaoRestaurante.Web.Controllers
         [HttpGet]
         public ActionResult DetalhesMesa(int id)
         {
-            var conta = _unit.ContaRepository.BuscarPor(x => x.MesaId == id);
-            if (conta.Count != 1)
+            var contasAbertas = _unit.ContaRepository.BuscarPor(x => x.Pago == false);
+            var contas = contasAbertas.Where(x => x.MesaId == id).ToList();
+            if (contas.Count != 1)
             {
                 return HttpNotFound();
             }
-            
-            return View(Mapper.Map<ContaViewModel>(conta.First()));
+            var conta = contas.First();
+            conta.Fechada = true;
+            _unit.ContaRepository.Alterar(conta);
+            _unit.Salvar();
+            return View(Mapper.Map<ContaViewModel>(conta));
         }
+
         #endregion GET
+
+        [HttpGet]
+        public ActionResult FecharConta(int id)
+        {
+            var mesa = id;
+            var contasAbertas = _unit.ContaRepository.BuscarPor(x => x.Pago == false);
+            var contas = contasAbertas.Where(x => x.MesaId == mesa).ToList();
+            if (contas.Count != 1)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+            var conta = contas.First();
+            conta.Pago = true;
+            _unit.ContaRepository.Alterar(conta);
+            _unit.Salvar();
+            return RedirectToAction("Index");
+        }
     }
 }
